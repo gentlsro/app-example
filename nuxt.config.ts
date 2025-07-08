98889c93242a540e3f0cafb06fb5d9471884d788
Kylian, Jakub <jacob.kylian@gmail.com>
2025-07-08 12:50:19 +0200

    chore: fix i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -35,10 +35,27 @@ export default defineNuxtConfig({
 
   // i18n
   i18n: {
+    langDir: '../i18n',
     compilation: {
       strictMessage: false,
       escapeHtml: false,
     },
+    locales: [
+      {
+        code: 'en-US',
+        file: `en-US.json`,
+        dateFormat: 'MM/DD/YYYY',
+        currency: 'USD',
+        icon: 'i-emojione:flag-for-united-kingdom',
+      },
+      {
+        code: 'cs-CZ',
+        file: `cs-CZ.json`,
+        dateFormat: 'DD.MM.YYYY',
+        currency: 'CZK',
+        icon: 'i-emojione:flag-for-czechia',
+      },
+    ],
   },
 
   unocss: {
